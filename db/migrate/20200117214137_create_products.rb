6318f901406f140c1154210103e891988c386f44
Colman Currie <curriecolman@gmail.com>
2020-01-17 13:51:43 -0800

    add migrate specs

---
+++ db/migrate/20200117214137_create_products.rb
@@ -0,0 +1,12 @@
+class CreateProducts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :products do |t|
+      # Here we'll put code that specifies the changes we want to make to our database.
+      t.column(:name, :string)
+      t.column(:cost, :integer)
+      t.column(:country_of_origin, :string)
+
+      t.timestamps()
+    end
+  end
+end
