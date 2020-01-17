53f14df7c9cb420218219cb8a403a236ef7f0613
Colman Currie <curriecolman@gmail.com>
2020-01-17 14:08:48 -0800

    add faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Product.destroy_all
+
+
+50.times do |index|
+  Product.create! :reviews => Faker::Commerce.product_name(max: 50)
+end
+
+p "Created #{Product.count} product"
