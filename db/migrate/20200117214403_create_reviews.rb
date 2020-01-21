17d4c06b483448da993057488f126d80673bc114
Colman Currie <curriecolman@gmail.com>
2020-01-20 23:05:21 -0800

    update some things

--- db/migrate/20200117214403_create_reviews.rb
+++ db/migrate/20200117214403_create_reviews.rb
@@ -3,7 +3,7 @@ class CreateReviews < ActiveRecord::Migration[5.2]
     create_table :reviews do |t|
       t.column(:author, :string)
       t.column(:rating, :integer)
-      t.column(:content_body, :integer)
+      t.column(:content_body, :text)
 
       t.timestamps
     end
--- db/migrate/20200117214531_add_foreign_key_for_reviews.rb
+++ db/migrate/20200117214531_add_foreign_key_for_reviews.rb
@@ -1,4 +1,4 @@
-class AddForeignKeyForSongs < ActiveRecord::Migration[6.0]
+class AddForeignKeyForReviews < ActiveRecord::Migration[6.0]
   def change
     add_foreign_key :reviews, :products
   end
