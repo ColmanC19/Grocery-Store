951a1ed4f45c24e1f8d13b47587262cff0e0ac15
Colman Currie <curriecolman@gmail.com>
2020-01-24 14:24:58 -0800

    mod foreign_key_for_reviews

--- db/migrate/20200122023820_create_products.rb
+++ db/migrate/20200122023820_create_products.rb
@@ -1,12 +1,9 @@
 class CreateProducts < ActiveRecord::Migration[5.2]
   def change
     create_table :products do |t|
-      # Here we'll put code that specifies the changes we want to make to our database.
       t.column(:name, :string)
       t.column(:cost, :integer)
       t.column(:country_of_origin, :string)
-
-      t.timestamps()
     end
   end
 end
