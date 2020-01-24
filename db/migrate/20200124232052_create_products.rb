b180ff10b2829c0f6e9dd7374079746ebfe60afe
Colman Currie <curriecolman@gmail.com>
2020-01-24 15:35:43 -0800

    edit a few things

--- db/migrate/20200124232052_create_products.rb
+++ db/migrate/20200124232052_create_products.rb
@@ -1,9 +1,13 @@
 class CreateProducts < ActiveRecord::Migration[5.2]
   def change
     create_table :products do |t|
+      # Here we'll put code that specifies the changes we want to make to our database.
       t.column(:name, :string)
       t.column(:cost, :integer)
       t.column(:country_of_origin, :string)
+      # t.integer :product_id
+
+      t.timestamps()
     end
   end
 end
