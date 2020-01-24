dda7cde789b69a7293d98f76cc0bf741640f7063
Colman Currie <curriecolman@gmail.com>
2020-01-24 15:34:19 -0800

    update product.rb

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,16 +6,16 @@ class Product < ApplicationRecord
     .group("products.id")
     .order("reviews_count DESC")
     )}
-  scope :three_most_recent, -> { order(created_at: :desc).limit(3)}
-  scope :made_in_the_usa, -> { where("country_of_origin like ?", "%United States%")}
-  validates :name, presence: true
-  validates :cost, presence: true
-  validates :country_of_origin, presence: true
-  before_save(:titleize_product)
+    scope :three_most_recent, -> { order(created_at: :desc).limit(3)}
+    scope :made_in_the_usa, -> { where("country_of_origin like ?", "%United States%")}
+    validates :name, presence: true
+    validates :cost, presence: true
+    validates :country_of_origin, presence: true
+    before_save(:titleize_product)
 
-private
-  def titleize_product
-    self.name = self.name.titleize
-    self.country_of_origin = self.country_of_origin.titleize()
+    private
+    def titleize_product
+      self.name = self.name.titleize
+      self.country_of_origin = self.country_of_origin.titleize()
+    end
   end
-end
