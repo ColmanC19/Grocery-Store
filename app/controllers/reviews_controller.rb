5b7760d9938294c9d9704ae955b77cf855f28c86
Colman Currie <curriecolman@gmail.com>
2020-01-26 21:27:04 -0800

    update review controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,16 +1,20 @@
 class ReviewsController < ApplicationController
-  before_action :authorize, only: [:new, :create, :edit, :update, :destroy]
+  # before_action :authorize, only: [:new, :create, :edit, :update, :destroy]
+
+
+  def save
+  end
 
   def new
     @product = Product.find(params[:product_id])
-    @employee = @product.employees.new
+    @review = @product.reviews.new
     render :new
   end
 
   def create
     @product = Product.find(params[:product_id])
-    @employee = @product.employees.new(employee_params)
-    if @employee.save
+    @review = @product.reviews.new(review_params)
+    if @review.save
       redirect_to product_path(@product)
     else
       render :new
@@ -19,34 +23,34 @@ class ReviewsController < ApplicationController
 
   def show
     @product = Product.find(params[:product_id])
-    @employee = Review.find(params[:id])
+    @review = Review.find(params[:id])
     render :show
   end
 
   def edit
     @product = Product.find(params[:product_id])
-    @employee = Review.find(params[:id])
+    @review = Review.find(params[:id])
     render :edit
   end
 
   def update
-    @employee = Review.find(params[:id])
-    if @employee.update(employee_params)
-      redirect_to product_path(@employee.product)
+    @review = Review.find(params[:id])
+    if @review.update(review_params)
+      redirect_to product_path(@review.product)
     else
       render :edit
     end
   end
 
   def destroy
-    @employee = Review.find(params[:id])
-    @employee.destroy
-    redirect_to product_path(@employee.product)
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to product_path(@review.product)
   end
 
   private
-    def employee_params
-      params.require(:employee).permit(:name)
+    def review_params
+      params.require(:review).permit(:name)
     end
 
 end
