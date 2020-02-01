0173d0e286c8c8f5f14658a4bc4c0ace914193c1
Colman Currie <curriecolman@gmail.com>
2020-02-01 15:40:59 -0800

    update controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,13 @@
 class ApplicationController < ActionController::Base
+
   protect_from_forgery with: :exception
   helper_method :current_user
+
+  def current_user
+    if session[:user_id]
+      @current_user ||= User.find(session[:user_id])
+    end
+  end
 
   def authorize
     if !current_user
@@ -9,9 +16,4 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  def current_user
-    if session[:user_id]
-      @current_user ||= User.find(session[:user_id])
-    end
-  end
 end
