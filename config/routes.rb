2624046071bcc171e799b58ddec436e3eb151478
Colman Currie <curriecolman@gmail.com>
2020-02-01 15:41:30 -0800

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,14 @@
 Rails.application.routes.draw do
-  resources :sessions
+  root to: 'sessions#new'
+  resources :products do
+    resources :reviews
+  end
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
 
   get '/signin' => 'sessions#new'
   post '/signin' => 'sessions#create'
   get '/signout' => 'sessions#destroy'
-
-  resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'products#index'
-  resources :products do
-    resources :reviews
-end
 end
 
 
