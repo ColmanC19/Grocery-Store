36622934ddb6bbc5503b8b2d2d49ba90e237d379
Colman Currie <curriecolman@gmail.com>
2020-02-01 13:49:20 -0800

    recreate a more effective database

--- db/migrate/20200201214352_create_reviews.rb
+++ db/migrate/20200201214352_create_reviews.rb
@@ -2,9 +2,9 @@ class CreateReviews < ActiveRecord::Migration[5.2]
   def change
     create_table :reviews do |t|
       t.column(:author, :string)
+      t.column(:content_body, :string)
       t.column(:rating, :integer)
-      t.column(:content_body, :text)
-      # t.column(:product_id, :integer)
+      t.column(:product_id, :integer)
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_27_063901) do
+ActiveRecord::Schema.define(version: 2020_02_01_214647) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,19 +25,9 @@ ActiveRecord::Schema.define(version: 2020_01_27_063901) do
 
   create_table "reviews", force: :cascade do |t|
     t.string "author"
+    t.string "content_body"
     t.integer "rating"
-    t.text "content_body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.integer "product_id"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "email"
-    t.string "password_hash"
-    t.string "password_salt"
-    t.string "user_name"
-    t.boolean "admin"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
